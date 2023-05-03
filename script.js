fe0e18c3e5ced75286f8e55ca9c5e6b64ffbddde
slifer <miguezpzmx@gmail.com>
2023-05-02 20:06:47 -0600

    Agregué los archivos html y js, también he agregado algunas mascotas a la clase de Batalla de mascotas

---
+++ script.js
@@ -0,0 +1,19 @@
+class Batalla_de_mascotas {
+    constructor(nombre, img, id, especialidad) {
+        this.nombre = nombre;
+        this.img = img;
+        this.id = id;
+        this.especialidad = especialidad;
+        this.ataques = [];
+    }
+}
+
+let mascotas = [];
+let peluchin = new Batalla_de_mascotas("Peluchin", "null", "peluchin_id", "piedra");
+let sazu = new Batalla_de_mascotas("Sazu", "null", "sazu_id", "piedra");
+let aren = new Batalla_de_mascotas("Aren", "null", "aren_id", "piedra");
+let oreo = new Batalla_de_mascotas("Oreo", "null", "oreo_id", "papel");
+let harry = new Batalla_de_mascotas("Harry", "null", "harry_id", "papel");
+let toby = new Batalla_de_mascotas("Toby", "null", "toby_id", "papel");
+
+// Opciones: Gato Huaniqueo, Gato Olga, kong
